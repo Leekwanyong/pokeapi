7dde4ba9321824094dde31f7e8249c459e384aff
Leekwanyong <dlrkdid12@gmail.com>
2025-02-06 19:48:25 +0900

    feat: 포켓몬APi getFlavorTextData, getGenusTypeData, id 추가

--- src/api/pokemonApi.ts
+++ src/api/pokemonApi.ts
@@ -8,10 +8,12 @@ interface KoreaPokemonNameType {
 }
 
 interface FlavorTextData  {
+  id: number
   flavor_text: string;
 }
 
 interface GenusType {
+  id: number;
   genus: string;
 }
 
@@ -32,9 +34,12 @@ export const getFlavorTextData = async (): Promise<FlavorTextData[]> => {
   const speciesResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon-species?limit=150`)
   const speciesFilter = await Promise.all(speciesResponse.data.results.map((item) => axios.get(item.url)));
   const koreaPokemonName= speciesFilter.map((item) => item.data.flavor_text_entries.find(item => item.language.name === 'ko'));
-  const flavorTextData: FlavorTextData[] = koreaPokemonName.map(item =>   ({
-    flavor_text: item.flavor_text
+  const flavorTextData: FlavorTextData[] = koreaPokemonName.map((item, index) =>   ({
+    id: index,
+    flavor_text: item.flavor_text,
   }))
+
+  console.log(flavorTextData);
 
   return flavorTextData
 }
@@ -43,7 +48,8 @@ export const getGenusTypeData = async (): Promise<GenusType[]>  => {
   const speciesResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon-species?limit=150`)
   const speciesFilter = await Promise.all(speciesResponse.data.results.map((item) => axios.get(item.url)));
   const koreaPokemonName = speciesFilter.map((item) => item.data.genera.find(item => item.language.name === 'ko'));
-  const genusTypeData: GenusType[] = koreaPokemonName.map(item => ({
+  const genusTypeData: GenusType[] = koreaPokemonName.map((item, index) => ({
+    id: index,
     genus: item.genus
   }))
   return genusTypeData
