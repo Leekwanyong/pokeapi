8f4b4bf330946af92cfe16ce678c977b1db19966
Leekwanyong <dlrkdid12@gmail.com>
2025-02-06 19:50:21 +0900

    refactor: 포켓몬APi getFlavorTextData, getGenusTypeData,getPokemon, 필요없는 type 삭제

--- src/api/pokemonApi.ts
+++ src/api/pokemonApi.ts
@@ -25,7 +25,7 @@ export const getPokemonList = async () => {
 export const getPokemon = async (): Promise<KoreaPokemonNameType[] > => {
   const speciesResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon-species?limit=150`)
   const speciesFilter = await Promise.all(speciesResponse.data.results.map((item) => axios.get(item.url)));
-  const koreaPokemonName: KoreaPokemonNameType[] = speciesFilter.map((item) => item.data.names.find(item => item.language.name === 'ko'));
+  const koreaPokemonName = speciesFilter.map((item) => item.data.names.find(item => item.language.name === 'ko'));
 
   return koreaPokemonName
 }
@@ -34,7 +34,7 @@ export const getFlavorTextData = async (): Promise<FlavorTextData[]> => {
   const speciesResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon-species?limit=150`)
   const speciesFilter = await Promise.all(speciesResponse.data.results.map((item) => axios.get(item.url)));
   const koreaPokemonName= speciesFilter.map((item) => item.data.flavor_text_entries.find(item => item.language.name === 'ko'));
-  const flavorTextData: FlavorTextData[] = koreaPokemonName.map((item, index) =>   ({
+  const flavorTextData = koreaPokemonName.map((item, index) =>   ({
     id: index,
     flavor_text: item.flavor_text,
   }))
@@ -48,7 +48,7 @@ export const getGenusTypeData = async (): Promise<GenusType[]>  => {
   const speciesResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon-species?limit=150`)
   const speciesFilter = await Promise.all(speciesResponse.data.results.map((item) => axios.get(item.url)));
   const koreaPokemonName = speciesFilter.map((item) => item.data.genera.find(item => item.language.name === 'ko'));
-  const genusTypeData: GenusType[] = koreaPokemonName.map((item, index) => ({
+  const genusTypeData = koreaPokemonName.map((item, index) => ({
     id: index,
     genus: item.genus
   }))
