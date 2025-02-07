f1f801fd05cc3c3839105d69e9badc17b8d9424e
Leekwanyong <dlrkdid12@gmail.com>
2025-02-08 00:29:55 +0900

    feat: QueryClientProvider 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
-import Card from './components/Card.tsx'
+import Card from './components/Card.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-
+const queryClient = new QueryClient();
 
 function App() {
-
-
-
   return (
-    <div>
-      <Card/>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Card />
+    </QueryClientProvider>
   );
 }
 
