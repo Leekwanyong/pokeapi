149b787ff681e500bd1003c402acf620252ebb38
Leekwanyong <dlrkdid12@gmail.com>
2025-02-07 17:19:23 +0900

    fix: modal on/off 스크롤 시 안 닫히는 버그 수정

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
 import {
   FlavorTextData,
   GenusType,
@@ -41,8 +40,11 @@ const Modal = ({ id, check, onClick }: Props) => {
   const findGenusData = modalData.map((item) =>
     item.genus.find((v) => v.id === id),
   );
-  console.log(check, onClick);
-  return ReactDOM.createPortal(
+
+  // 여기서 state를 하나 더 만들어서 modal에 클릭을 관리하는 것 보다 기존 modal을 받아와서 업데이트 하는 것이 더 좋음
+  // map 함수로 두 개를 돌렸는데 이게 더 나은 방법인가? 하나의 배열로 만들어서 해봤지만 같은 dom 요소가 두개가 생성 되었다.
+  // 이는 그 전에 있는 것을 가져와서 그런데 map 함수를 두 개 쓰는 방법 말고는 해결 방법을 잘 몰르겠다...
+  return (
     <>
       {check && (
         <Wrapper onClick={() => onClick()}>
@@ -67,14 +69,13 @@ const Modal = ({ id, check, onClick }: Props) => {
           </Ul>
         </Wrapper>
       )}
-    </>,
-
-    document.body,
+    </>
   );
 };
 
 export default Modal;
 
+// height 범위 떄문에 범위를 넘어가서 클릭하면은 안 닫힘
 const Wrapper = styled.div`
   position: absolute;
   width: 100vw;
@@ -85,10 +86,10 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  position: fixed;
 `;
 
 const Ul = styled.ul`
-  position: fixed;
   list-style: none;
   text-align: center;
   display: flex;
