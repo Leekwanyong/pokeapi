4dd12a1f0abc5d0ef19906602a6f7b8e85abf663
Leekwanyong <dlrkdid12@gmail.com>
2025-02-07 18:54:25 +0900

    feat: type 정리

---
+++ src/types/PoketApi/pokemonApiType.ts
@@ -0,0 +1,17 @@
+export interface KoreaPokemonNameType {
+  language: {
+    name: string;
+    url: string;
+  };
+  name: string;
+}
+
+export interface FlavorTextData {
+  id: number;
+  flavor_text: string;
+}
+
+export interface GenusType {
+  id: number;
+  genus: string;
+}
