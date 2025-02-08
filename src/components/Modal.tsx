21b35e1ea4ae4a00a19128e2b257cd8c788b5c88
Leekwanyong <dlrkdid12@gmail.com>
2025-02-09 03:46:23 +0900

    refactor: 기존 useEffect 비동기 호출을 useQuery로 리팩토링하여 코드 간소화

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,36 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Props } from '../types/Card/cardType';
-import { ModalData } from '../types/Modal/modalType';
-import {
-  FlavorTextData,
-  GenusType,
-  getFlavorTextData,
-  getGenusTypeData,
-} from '../api/pokemonApi.ts';
+import { GenusType, getFlavorTextData } from '../api/pokemonApi.ts';
+import { useQuery } from '@tanstack/react-query';
+import Loading from './Loading.tsx';
 
 const Modal = ({ id, check, onClick }: Props) => {
-  const [modalData, setModalData] = useState<ModalData[]>([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const flavorTextData = await getFlavorTextData();
-        const genusData = await getGenusTypeData();
-        setModalData([{ flavorText: flavorTextData, genus: genusData }]);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    fetchData();
-  }, []);
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['pokemonDetailData'],
+    queryFn: async () => {
+      const response = await getFlavorTextData();
+      return response;
+    },
+  });
 
-  const findFlavorTextData = modalData.map((item) =>
-    item.flavorText.find((v) => v.id === id),
+  const findFlavorTextData = (data?.flavorTextData ?? []).filter(
+    (item) => item.id === id,
   );
 
-  const findGenusData = modalData.map((item) =>
-    item.genus.find((v) => v.id === id),
+  const findGenusData = (data?.genusTypeData ?? []).filter(
+    (item) => item.id === id,
   );
+
+  if (isLoading) return <Loading loading={isLoading} />;
+  if (isError) return <p>{isError}</p>;
 
   // 여기서 state를 하나 더 만들어서 modal에 클릭을 관리하는 것 보다 기존 modal을 받아와서 업데이트 하는 것이 더 좋음
   // map 함수로 두 개를 돌렸는데 이게 더 나은 방법인가? 하나의 배열로 만들어서 해봤지만 같은 dom 요소가 두개가 생성 되었다.
